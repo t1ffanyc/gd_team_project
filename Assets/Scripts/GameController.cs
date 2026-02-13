74666e825b455daca753ff03104c24d100d4ac7d
t1ffanyc <tchen1515@gmail.com>
2026-02-12 16:04:01 -0800

    rotate gravity physics

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameController : MonoBehaviour
 {
+
+    private int gravityState = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +16,30 @@ public class GameController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnRotateLeft(InputValue value)
+    {
+        Debug.Log("rotate left");
+        gravityState = (gravityState + 1) % 4;
+        RotateGravity();
+    }
+
+    void OnRotateRight(InputValue value)
+    {
+        Debug.Log("rotate right");
+        gravityState = (gravityState + 3) % 4;
+        RotateGravity();
+    }
+
+    void RotateGravity()
+    {
+        switch (gravityState)
+        {
+            case 0: Physics2D.gravity = new Vector2(0, -9.81f); break; // down
+            case 1: Physics2D.gravity = new Vector2(9.81f, 0); break;  // right
+            case 2: Physics2D.gravity = new Vector2(0, 9.81f); break;  // up
+            case 3: Physics2D.gravity = new Vector2(-9.81f, 0); break; // left
+        }
     }
 }
