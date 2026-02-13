6548a807094df9c2ebfec0f5db03d0cb4f3f9c6c
t1ffanyc <tchen1515@gmail.com>
2026-02-12 16:15:11 -0800

    camera rotation

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class GameController : MonoBehaviour
 {
 
     private int gravityState = 0;
+    private bool isRotating = false;
+    [SerializeField] private float rotationDuration;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,26 +23,51 @@ public class GameController : MonoBehaviour
 
     void OnRotateLeft(InputValue value)
     {
+        if(isRotating) return;
         Debug.Log("rotate left");
         gravityState = (gravityState + 1) % 4;
-        RotateGravity();
+        RotateWorld();
     }
 
     void OnRotateRight(InputValue value)
     {
+        if(isRotating) return;
         Debug.Log("rotate right");
         gravityState = (gravityState + 3) % 4;
-        RotateGravity();
+        RotateWorld();
     }
 
-    void RotateGravity()
+    void RotateWorld()
     {
-        switch (gravityState)
+        float targetAngle = gravityState * 90f;
+
+        Vector2 baseGravity = new Vector2(0, -9.81f);
+        Physics2D.gravity = Quaternion.Euler(0, 0, targetAngle) * baseGravity;
+
+        StartCoroutine(SmoothRotate(targetAngle));
+    }
+
+    IEnumerator SmoothRotate(float targetAngle)
+    {
+        isRotating = true;
+
+        Transform cam = Camera.main.transform;
+
+        Quaternion startRotation = cam.rotation;
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, targetAngle);
+
+        float time = 0f;
+
+        while (time < rotationDuration)
         {
-            case 0: Physics2D.gravity = new Vector2(0, -9.81f); break; // down
-            case 1: Physics2D.gravity = new Vector2(9.81f, 0); break;  // right
-            case 2: Physics2D.gravity = new Vector2(0, 9.81f); break;  // up
-            case 3: Physics2D.gravity = new Vector2(-9.81f, 0); break; // left
+            time += Time.deltaTime;
+            float t = time / rotationDuration;
+
+            cam.rotation = Quaternion.Lerp(startRotation, targetRotation, t);
+            yield return null;
         }
+
+        cam.rotation = targetRotation;
+        isRotating = false;
     }
 }
